f0098135eb0f9cd8ba6943a17ad594b8c87c5b7f
Miklos Koren <miklos.koren@gmail.com>
2022-01-18 16:52:36 +0100

    1. Move date extraction to function. 2. Write feet to meters conversion

--- date.py
+++ date.py
@@ -1,7 +1,14 @@
 date = '2022-01-18'
 
-year = int(date[0:4])
-month = int(date[5:7])
-day = int(date[-2:])
+def compute_tomorrow(date):
+    year = date[0:4]
+    month = date[5:7]
+    day = int(date[-2:])
+    return year + '-' + month + '-' + str(day + 1)
 
-print('Next class is on ', year, month, day + 1)
+print('Next class is on ', compute_tomorrow(date))
+
+def convert_to_meters(number):
+    return number * 0.3048
+
+print('200 feet in meters =', convert_to_meters(200.0))
