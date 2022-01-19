5cda45d9943f165093d7741dfdd8120faf820902
Miklos Koren <miklos.koren@gmail.com>
2022-01-19 16:26:34 +0100

    If practice

--- practice.py
+++ practice.py
@@ -16,3 +16,16 @@ print('Now I am done.')
 
 print('1000 inches in meters are', convert_feet_to_meters(convert_inches_to_feet(1000.0)))
 print('1000 inches in meters are', convert_inches_to_meters(1000.0))
+
+# If statements
+if distance_meter > 10.0:
+    print('Distance is greater than 10 meters.')
+else:
+    print('Distance is not than great.')
+
+if False:
+    print('It is false.')
+else:
+    print('it is not.')
+
+print(distance_meter > 10.0)
